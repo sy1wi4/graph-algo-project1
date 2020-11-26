6b2a49543b76512b125843eac05503d81d6694c0
Sylwia Marek <sylwia1301@onet.pl>
2020-11-26 23:59:59 +0100

    input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    return 0;
+    // liczba turniejów
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        // budżet, liczba zawodników
+        int B,n;
+        cin >> B >> n;
+        for (int j = 0; j < n * (n - 1) / 2; j++) {
+            // x gra przeciwko y, zwycięża w, łapówka b
+            int x, y, w, b;
+            cin >> x >> y >> w >> b;
+        }
+    }
+
 }
